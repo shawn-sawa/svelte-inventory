0eaab1558f8507759380a0b02e4cabac7639a589
ravenallan <ravenallan7@gmail.com>
2021-05-06 10:45:31 +0200

    Added `rollup`, removed `webpack`, app is working

--- src/main.js
+++ src/main.js
@@ -1,11 +1,51 @@
 
-import App from './App.svelte';
+import Home from './module/Home.svelte';
 
-const app = new App({
+const App = {
+
+    init: function() {
+        console.log('=> App.init()');
+
+        // Initialize Application User
+        // App.User = {};
+
+        // Initialize Application State
+        // App.State = {};
+
+        // Initialize Application Globals/Constants
+        // App['TIMEOUT'] = 1000 * 60 * 30;
+
+        App.load();
+    },
+
+    load: function() {
+        console.log('=> App.load()');
+
+        const config = {
     target: document.body,
     props: {
-        name: 'world'
+        name: 'Shawn'
     }
-});
+};
 
-export default app;
+        App.render(config).exec();
+    },
+
+    exec: function() {
+        console.log('=> App.exec()');
+
+        // Global Event Handlers go here...
+    },
+
+    render: function(content) {
+        App.Home = new Home(content);
+
+        return this;
+    }
+};
+
+window.onload = () => {
+    App.init();
+};
+
+// export default App;
